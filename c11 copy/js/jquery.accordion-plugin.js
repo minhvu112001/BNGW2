d1454aaf4b60e9a05ee80e0eab3d46029e866eb5
Vu Minh <minhvu112001@gmail.com>
2021-06-07 19:56:48 +0700

    task_18_BNGW

---
+++ c11 copy/js/jquery.accordion-plugin.js
@@ -0,0 +1,13 @@
+(function ($){
+    $.fn.accordion = function (speed){
+        this.on('click', 'accordion-control', function (e){
+            e.preventDefault();
+            $(this)
+                .next('.accordion-panel')
+                .not(':animated')
+                .slideToggle(speed);
+        });
+        return this;
+
+    };
+}(jQuery));
